394501a69d120bc918a618946f8e8f8cc3fa662c
Daniel Lublin <daniel@lublin.se>
2022-10-18 12:44:11 +0200

    Add test-loop.sh script

---
+++ test-loop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+portflag=  # use auto-detect
+#portflag="--port /dev/ttyACM0"
+
+speedflag=  # use default
+#speedflag="--speed 62500"
+
+cd "${0%/*}" || exit 1
+
+#make clean
+make
+
+./runapp $portflag $speedflag --file apps/signerapp/app.bin || exit 1
+
+c=0
+t=$(date +%s)
+while true; do
+  # 128 bytes becomes 1 msg with 127 bytes and 1 msg with 1 byte
+  dd 2>/dev/null if=/dev/urandom of=rand1k bs=128 count=1
+  if ! ./tk-sign $portflag $speedflag --file ./rand1k; then
+    exit 1
+  fi
+  (( c++ ))
+  printf "loop count: %d, seconds passed: %d\n" "$c" "$(($(date +%s) - t))"
+done
