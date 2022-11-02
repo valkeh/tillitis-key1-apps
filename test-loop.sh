998544c66dca3632b9feb4919d2546a333f07232
Matt Mets <matt@blinkinlabs.com>
2022-11-02 18:22:24 +0100

    Create unique temporary files for each test-loop run

--- test-loop.sh
+++ test-loop.sh
@@ -10,19 +10,23 @@ fi
 speedflag=  # use default
 #speedflag="--speed 62500"
 
+filename=`mktemp`
+echo $filename
+
 cd "${0%/*}" || exit 1
 
 #make clean
-make
+#make
 
 ./runapp $portflag $speedflag --file apps/signerapp/app.bin || exit 1
+
 
 c=0
 t=$(date +%s)
 while true; do
   # 128 bytes becomes 1 msg with 127 bytes and 1 msg with 1 byte
-  dd 2>/dev/null if=/dev/urandom of=rand1k bs=128 count=1
-  if ! ./tk-sign $portflag $speedflag --file ./rand1k; then
+  dd 2>/dev/null if=/dev/urandom of=$filename bs=128 count=1
+  if ! ./tk-sign $portflag $speedflag --file $filename; then
     exit 1
   fi
   (( c++ ))
